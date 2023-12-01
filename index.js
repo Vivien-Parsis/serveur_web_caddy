0643a0b2487c3f0804a24da40d76c5bb17d6104b
VIvien-Parsis <vivienparsis.perso@gmail.com>
2023-12-01 08:16:54 +0100

    added alpinejs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fastify = require('fastify')({logger: true})
 const path = require('node:path');
 const crypto = require('crypto');
+const fs = require('node:fs');
 
 fastify.register(require('@fastify/mongodb'), {
   forceClose: true,
@@ -8,7 +9,7 @@ fastify.register(require('@fastify/mongodb'), {
 })
 fastify.register(require('@fastify/jwt'),{
     secret : 'supersecret'
-})
+});
 fastify.register(require('@fastify/static'),{ 
     root: path.join(__dirname),
     prefix: '/',
@@ -18,13 +19,23 @@ fastify.register(require('@fastify/formbody'));
 
 fastify.decorate("authenticate", async (request, reply) => {
     try {
-      await request.jwtVerify()
+        await request.jwtVerify()
     } catch (err) {
-      reply.send(err)
+        reply.send(err)
     }
-  }
-)
-
+})
+fastify.register(require('@fastify/secure-session'), {
+    // the name of the attribute decorated on the request-object, defaults to 'session'
+    sessionName: 'session',
+    // the name of the session cookie, defaults to value of sessionName
+    cookieName: 'token',
+    // adapt this to point to the directory where secret-key is located
+    key: fs.readFileSync(path.join(__dirname, 'secret-key')),
+    cookie: {
+        path: '/'
+        // options for setCookie, see https://github.com/fastify/fastify-cookie
+    }
+  })
 fastify.get('/', (request, reply) => {
     reply.header('Content-Type', 'text/HTML')
     reply.sendFile("index.html");
@@ -52,8 +63,10 @@ fastify.post('/messages',  async (req, reply) => {
 })
 
 fastify.post('/signin', async (req,reply)=>{
-    const mail = req.body.mail;
-    const password = req.body.password;
+    reply.header("Access-Control-Allow-Origin", "*");
+    const json = JSON.parse(req.body);
+    const mail = json.mail;
+    const password = json.password;
     if(mail.trim()==="" || password.trim()===""){
         reply.send("err");
     }
@@ -63,9 +76,10 @@ fastify.post('/signin', async (req,reply)=>{
         const users = await caddy.findOne({mail:mail,password:hash});
         if(users!=null){
             const token = fastify.jwt.sign({mail:users.mail});
-            return {token}
+            req.session.set('token',{mail:users.mail});
+            return {token};
         }
-        reply.send("unknown")
+        return reply.send("unknown")
     }catch(err){
         return err;
     }
@@ -92,8 +106,15 @@ fastify.post('/signup', async (req,reply)=>{
 fastify.get("/secret",{onRequest: [fastify.authenticate]},
     async function(request, reply) {
       return request.user
+})
+fastify.get("/private",(req,reply)=>{
+    const data = req.session.get('token')
+    if (!data) {
+    reply.code(404).send()
+    return
     }
-  )
+    reply.send(data)
+})
 fastify.listen({ port: 3000 }, (err, address) => {
     console.log(`listening to ${address}`)
     if (err) throw err;
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-to run with nodemon : \> npx nodemon -e js,html,json
+to run with node : \> node index.js
+<br>to run with nodemon : \> npx nodemon -e js,html,json
